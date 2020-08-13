e6a890e3365f93397844b232d0483e3063933a14
Sky <skylandwoll@gmail.com>
2020-08-13 00:50:55 -0400

    put up dev message in the meantime

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,23 +6,15 @@ import Layout from '../components/layout'
 export default function Home() {
   return (
     <div className="front">
-      <Layout>
-        <h1>Hello Gatsby! I'm Sky.</h1>
-        <p>What a beautiful world.</p>
-
-        <img src="https://source.unsplash.com/random/400x200" alt="" />
-        <p>
-          <Link to="/about">Learn a lil more.</Link>
-        </p>
+      
+        <h1>In development!</h1>
+        
         <p>
           <a href="https://html5zombo.com/" target="_blank" rel="noreferrer">
-            Go far away.
+            In the meantime, check out the best website in the world.
           </a>
         </p>
-        <p>
-          Need a guy? <Link to="/contact">Say howdy!</Link>
-        </p>
-      </Layout>
+        
     </div>
   )
 }
