a431e9410d43ed2bdf545d457b14074783eef234
Sky <skylandwoll@gmail.com>
2020-08-13 01:02:03 -0400

    preview commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,23 @@ import Layout from '../components/layout'
 export default function Home() {
   return (
     <div className="front">
-      
-        <h1>In development!</h1>
-        
+      <Layout>
+        <h1>Hello Gatsby! I'm Sky.</h1>
+        <p>What a beautiful world.</p>
+
+        <img src="https://source.unsplash.com/random/400x200" alt="" />
+        <p>
+          <Link to="/about">Learn a lil more.</Link>
+        </p>
         <p>
           <a href="https://html5zombo.com/" target="_blank" rel="noreferrer">
-            In the meantime, check out the best website in the world.
+            Go far away.
           </a>
         </p>
-        
+        <p>
+          Need a guy? <Link to="/contact">Say howdy!</Link>
+        </p>
+      </Layout>
     </div>
   )
 }
