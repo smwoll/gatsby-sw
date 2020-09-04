0e9ae7882263ceff18289a017bb36f5b73ae710f
Sky <skylandwoll@gmail.com>
2020-09-04 10:58:38 -0400

    sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,13 +11,15 @@ module.exports = {
     author: "Skyland Woll",
     titleTemplate: "%s · Skyland Woll · Front End Web Developer",
     description:
-      "Hi, I'm Skyland Woll. I'm a frontend web developer ready to design and build your next world-changing website.",
+      "Hi, I'm Skyland Woll. I'm a front end web developer ready to design and build your next world-changing website.",
     url: "https://skyland.dev", // No trailing slash allowed!
+    SiteUrl: "https://skyland.dev",
     image: "/skyland-dev-social.jpeg", // Path to your image you placed in the 'static' folder
     twitterUsername: "@therealskyland",
   },
   plugins: [
     `gatsby-plugin-scroll-reveal`,
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-sass`,
