532c7d720f49fbda9ac9e7f7bf63e4a455821b7f
Sky <skylandwoll@gmail.com>
2020-12-31 14:35:19 -0500

    add burger bullet

--- src/pages/how.js
+++ src/pages/how.js
@@ -1,143 +1,152 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react"
+import { Link } from "gatsby"
 import SEO from "../components/SEO"
-import Layout from '../components/layout'
+import Layout from "../components/layout"
 
 const HowPage = () => {
-    return (
-      <Layout>
-        <SEO title="How I Made This · Skyland Woll" />
-        <h1 data-sal="slide-down" data-sal-delay="0" data-sal-easing="ease">
-          How I made this website
-        </h1>
-        <blockquote
-          data-sal="slide-up"
-          data-sal-delay="300"
-          data-sal-easing="ease"
-        >
-          "If I have seen further, it is by standing upon the shoulders of
-          giants."
-          <footer>
-            <a
-              href="https://en.wikipedia.org/wiki/Standing_on_the_shoulders_of_giants"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              —Isaac Newton
-            </a>
-          </footer>
-        </blockquote>
-        <blockquote
-          data-sal="slide-up"
-          data-sal-delay="300"
-          data-sal-easing="ease"
-        >
-          "Woah."
-          <footer>
-            <a
-              href="https://www.youtube.com/watch?v=avjdKTqiVvQ"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              —The person next to the person that discovered fire. Probably.
-            </a>
-          </footer>
-        </blockquote>
-        <p data-sal="slide-up" data-sal-delay="400" data-sal-easing="ease">
-          I'm the second guy in the second quote, and I'm very grateful for the
-          developers that shared their ideas, their open-source tech, and their
-          time. I will list the resources, blogs, and videos that helped inspire
-          me as I worked to create my portfolio website. This list is a work in
-          progress, and I will continue to update it.
-        </p>
-        <ul data-sal="slide-up" data-sal-delay="500" data-sal-easing="ease">
-          <li>
-            <a
-              href="https://1linelayouts.glitch.me/"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              This brilliant demo of 1-line layouts with flex and grid.
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.blobmaker.app/"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              A beautiful baby blob maker.
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://tobiasahlin.com/"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              Lots of inspiration and a few tips from the gorgeous portfolio of
-              Tobias Ahlin.
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.bourbon.io/"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              A dash of bourbon.
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.gatsbyjs.com/"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              A whole lotta Gatsby.
-            </a>
-            <ul>
-              <li>
-                <a
-                  href="https://www.youtube.com/watch?v=8t0vNu2fCCM&t=11s"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  The Great Gatsby Bootcamp
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.gatsbyjs.com/tutorial/"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  Gatsby Tutorial
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a
-              href="https://www.react-spring.io/"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              Springs!
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://css-tricks.com/a-complete-guide-to-calc-in-css/"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              CSS Tricks of course.
-            </a>
-          </li>
-        </ul>
-      </Layout>
-    )
+  return (
+    <Layout>
+      <SEO title="How I Made This · Skyland Woll" />
+      <h1 data-sal="slide-down" data-sal-delay="0" data-sal-easing="ease">
+        How I made this website
+      </h1>
+      <blockquote
+        data-sal="slide-up"
+        data-sal-delay="300"
+        data-sal-easing="ease"
+      >
+        "If I have seen further, it is by standing upon the shoulders of
+        giants."
+        <footer>
+          <a
+            href="https://en.wikipedia.org/wiki/Standing_on_the_shoulders_of_giants"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            —Isaac Newton
+          </a>
+        </footer>
+      </blockquote>
+      <blockquote
+        data-sal="slide-up"
+        data-sal-delay="300"
+        data-sal-easing="ease"
+      >
+        "Woah."
+        <footer>
+          <a
+            href="https://www.youtube.com/watch?v=avjdKTqiVvQ"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            —The person next to the person that discovered fire. Probably.
+          </a>
+        </footer>
+      </blockquote>
+      <p data-sal="slide-up" data-sal-delay="400" data-sal-easing="ease">
+        I'm the second guy in the second quote, and I'm very grateful for the
+        developers that shared their ideas, their open-source tech, and their
+        time. I will list the resources, blogs, and videos that helped inspire
+        me as I worked to create my portfolio website. This list is a work in
+        progress, and I will continue to update it.
+      </p>
+      <ul data-sal="slide-up" data-sal-delay="500" data-sal-easing="ease">
+        <li>
+          <a
+            href="https://1linelayouts.glitch.me/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            This brilliant demo of 1-line layouts with flex and grid.
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.blobmaker.app/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            A beautiful baby blob maker.
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://tobiasahlin.com/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Lots of inspiration and a few tips from the gorgeous portfolio of
+            Tobias Ahlin.
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.bourbon.io/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            A dash of bourbon.
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.gatsbyjs.com/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            A whole lotta Gatsby.
+          </a>
+          <ul>
+            <li>
+              <a
+                href="https://www.youtube.com/watch?v=8t0vNu2fCCM&t=11s"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                The Great Gatsby Bootcamp
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.gatsbyjs.com/tutorial/"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                Gatsby Tutorial
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a
+            href="https://www.react-spring.io/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Springs!
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://jonsuh.com/hamburgers/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Yummy Burgers
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://css-tricks.com/a-complete-guide-to-calc-in-css/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            CSS Tricks of course.
+          </a>
+        </li>
+      </ul>
+    </Layout>
+  )
 }
 
 export default HowPage
